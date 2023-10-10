8a36624c946911c1e144b97eeb4fd81e43578020
Valeska Marques <valeskkitty@gmail.com>
2023-10-10 11:44:06 -0300

    feat: criação da clase Utilitarios e função dump

---
+++ src/Utilitarios.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Microblog;
+
+abstract class Utilitarios
+{
+    /**
+     * sobre o parâmetro $dados com o tipo array | bool
+     * quando um parâmetro pode receber tipos de dados diferentes de acordo
+     * com a chmada do método, usamos o pipe (|) para separar os tipos
+     */
+    public static function dump(array | bool $dados) : void
+    {
+        echo "<pre>";
+        var_dump($dados);
+        echo "</pre>";
+    }
+}
