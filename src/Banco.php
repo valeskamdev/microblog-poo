4ebd9dfb30e98b8bb12cdde8c64d5194c46a193d
Valeska Marques <valeskkitty@gmail.com>
2023-10-24 09:27:13 -0300

    refactor: ajustando apenas uma conexao

--- src/Banco.php
+++ src/Banco.php
@@ -7,20 +7,39 @@ abstract class Banco {
     private static string $usuario = "root";
     private static string $senha = "";
     private static string $banco = "microblog_valeska";
-    private static PDO $conexao; 
+
+    /**
+     * Operador '?' (nullable typehint) quando usado,
+     * indica que a propriedade/atributo da classe pode
+     * conter um valor null OU pode ser um tipo PDO.
+     *
+     * Nesse caso, a propriedade conexao é inicializada como nula,
+     * mas a partir do momento em que uma conexão é feita
+     * ela passa a valer PDO.
+     */
+    private static ?PDO $conexao = null;
 
     public static function conecta():PDO {
-        try {
-            self::$conexao = new PDO(
-                "mysql:host=".self::$servidor."; 
-                dbname=".self::$banco.";
+        /**
+         * Só conecte se não houver conexão..
+         * Se a conexão for nula, faça as ações do try/catch
+         */
+        if (self::$conexao === null) {
+            try {
+                self::$conexao = new PDO(
+                  "mysql:host=" . self::$servidor . "; 
+                dbname=" . self::$banco . ";
                 charset=utf8",
-                self::$usuario, 
-                self::$senha
-            );
-            self::$conexao->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (Exception $erro) {
-            die("Deu ruim: ".$erro->getMessage());
+                  self::$usuario,
+                  self::$senha
+                );
+                self::$conexao->setAttribute(
+                  PDO::ATTR_ERRMODE,
+                  PDO::ERRMODE_EXCEPTION
+                );
+            } catch (Exception $erro) {
+                die("Deu ruim: " . $erro->getMessage());
+            }
         }
         return self::$conexao;
     }
